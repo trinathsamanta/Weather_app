b59987c66946969d9f8d6a9156100c3ea5288d3c
trinathsamanta <57518157+trinathsamanta@users.noreply.github.com>
2020-04-16 17:04:29 +0530

    added redux_thunk

--- redux/store.js
+++ redux/store.js
@@ -1,7 +1,8 @@
 import { createStore, applyMiddleware } from "redux";
 import  rootReducer  from "./reducer/rootReducer";
 import { composeWithDevTools } from "redux-devtools-extension";
-import thunk from "redux-thunk";
+import {thunk} from "redux-thunk";
+
 import promiseMiddleware from "redux-promise-middleware";
 
 const composeEnhancerMiddleware = composeWithDevTools(
